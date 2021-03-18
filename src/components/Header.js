0b86772fb611a919101ebd33e3ee073e0f847e04
Hawleywoo <alhawley21@gmail.com>
2021-03-17 21:59:47 -0600

    Css changes and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className="header__wrap" >
             <header className="header">
-                <div className="header__logo" ><span>Logo</span></div>
+                <div className="header__logo" ><img className="header__logo--image" src="https://images.getbento.com/accounts/fb0d891f5aff4d57adccd1ede962ae20/media/images/89530PizzeriaColore-logo.png" alt="Pizzeria Colore Logo"/></div>
                 <NavBar />
             </header>
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaBars } from 'react-icons/fa'
 
 function NavBar() {
     return (
         <div className="header__menu">
             <a>Order</a>
             <a>About Us</a>
+            <div><FaBars /></div>
         </div>
     )
 }
